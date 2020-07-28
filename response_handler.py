01b2d8fe6fe74529f83d41a9154e0e685d7e27c6
Thomas <soler.thomas.06@gmail.com>
2020-07-28 14:43:00 +0200

    Modify name acquisition method and url
    The repo's name is more accessible than through a regex. The URL cannot be an
    API url, so it will just redirect to the repo's url (for now)

--- response_handler.py
+++ response_handler.py
@@ -15,8 +15,8 @@ class ResponseHandler:
     for notification in self.response:
       repo_info = notification['repository']
       repo_relation = notification['reason']
-      repo_name = re.sub( r'^https:\/\/api\.github\.com\/repos\/', '',repo_info["url"])
-      notif_url = notification['url']
+      repo_name = notification['repository']['full_name']
+      notif_url = "https://api.github.com/" + repo_name
       notif_details = notification['subject']
       notif_reason = ' '.join(re.findall('[A-Z][^A-Z]*', notif_details["type"]))
       notif_date = parser.isoparse(notification["updated_at"]).replace(tzinfo=None)
@@ -47,7 +47,7 @@ class ResponseHandler:
 
   def print_result(self):
     if self.notifications == []:
-      print("No new notifications", 'red')
+      print(f"\033[1;31;40m No new notifications in sight!")
     for notification in self.notifications:
       self.pretty_print_notification(notification)
 
